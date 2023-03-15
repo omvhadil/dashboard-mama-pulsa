6a7ed735bf0cd76d1569967981927b5139352436
omvhadil <fadil.rohman.327@gmail.com>
2023-03-15 16:40:04 +0700

    pwa

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,35 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA({ registerType: 'autoUpdate', manifest: {
+      name: 'Alabim',
+      short_name: 'Alabim',
+      theme_color: '#000000',
+      background_color: '#000000',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      lang: 'en',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ]}
+     }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
